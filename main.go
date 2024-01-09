701cb6242bd777df07c848643fdba3efeca1cce9
Anvarbek <121457366+anvarbeckk@users.noreply.github.com>
2024-01-09 23:28:19 +0500

    text editor has been created

---
+++ main.go
@@ -0,0 +1,26 @@
+// main.go
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: ./lex filename")
+		os.Exit(1)
+	}
+
+	filename := os.Args[1]
+	editor := NewEditor(filename)
+
+	if err := editor.InitScreen(); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to initialize screen: %s\n", err)
+		os.Exit(1)
+	}
+
+	defer editor.FiniScreen()
+
+	editor.Run()
+}
